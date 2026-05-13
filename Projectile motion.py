bfa8d4f8f05dafd14b37e83a285c825ec3383fd1
Amal Raj <sab.amalraj@gmail.com>
2026-05-13 16:41:45 +0530

    Initial projectile motion simulator

---
+++ Projectile motion.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt 
+import math 
+g = 9.8
+u = 20
+angle = 10
+angle_rad = math.radians(angle)
+x_values = []
+y_values = []
+for t in range(50):
+    time = t/10
+    x = u * math.cos(angle_rad) * time
+    y = (u * math.sin(angle_rad) * time) - ( 1/2 * g * ( time ** 2 ) )
+    if y >= 0:
+        x_values.append(x)
+        y_values.append(y)
+plt.plot(x_values,y_values)
+plt.xlabel("Horizontal Distance")
+plt.ylabel("Vertical Distance")
+plt.title("Projectile Motion")
+plt.show()
